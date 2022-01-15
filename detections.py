855da37b2aa8f4ced2821f1c71b149c59611cc5d
21pdavis <paulcdavis135@gmail.com>
2022-01-15 17:47:18 -0500

    First iteration of working face detection plus some minor refactors and finalized implementation of Detection class hierarchy

--- detections.py
+++ detections.py
@@ -1,27 +1,47 @@
-import cv2
-from tkinter import *
-from threading import Thread
-from PIL import ImageTk, Image
+import tkinter as tk
 from abc import ABC, abstractmethod
+from threading import Thread
+
+import cv2
 import numpy as np
+from PIL import Image, ImageOps, ImageTk
 
 
-# TODO: Implement inheritance structure and basic initialization code for detections
 class Detection(ABC):
+    @property
     @abstractmethod
-    def _detect(self) -> None:
+    def run_detections(self) -> bool:
+        """ Getter for the bool flag used to start and stop detections """
+
+    @run_detections.setter
+    @abstractmethod
+    def run_detections(self, run_detections: bool) -> None:
+        """ Setter for the bool flag used to start and stop detections """
+
+    @abstractmethod
+    def _detect(self, webcam_feed: cv2.VideoCapture) -> None:
         """ Begin image processing and detection
 
         Use opencv and/or mediapipe pre-made solutions to run real-time
         detection algorithms based on which kind of detections the user
         specifies.
 
-        Will then convert the final img ndarrays into PhotoImage objects
-        to post to the GUI
+        Will then convert the final img NumPy arrays into PhotoImage
+        objects to post to the GUI
         """
 
     @abstractmethod
-    def _to_photo_image(self, rgb_array: np.ndarray) -> PhotoImage:
+    def _analyze_image(self, webcam_feed: cv2.VideoCapture) -> np.ndarray:
+        """ Reads and performs detections on the current video feed frame
+
+        Implementation of this method will differ between different
+        types of detections (e.g., face vs. hand detection).
+
+        Returns the img object NumPy array with detections drawn on
+        """
+
+    @abstractmethod
+    def _to_photo_image(self, img_array: np.ndarray) -> tk.PhotoImage:
         """ Converts image with drawn-on detections to usable PhotoImage object
 
         Takes in 3D np ndarray of RGB values and performs necessary
@@ -30,44 +50,94 @@ class Detection(ABC):
         """
 
     @abstractmethod
-    def _update_frame(self, final_img: PhotoImage) -> None:
-        """ Update the image label to simulate live video
-
-        Difficult part of this task is updating the video frame label
-        in a way that is compatible with the thread of the GUI and the
-        thread of the detections
-        """
+    def _update_frame(self) -> None:
+        """ Update the image label with the current frame with detections drawn on """
 
 
 class FaceDetection(Detection):
     FACE_CASCADE = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
 
-    def __init__(self, video_frame: Label) -> None:
-        self._detection_thread = Thread(target=self._detect)
-        self._video_frame = video_frame
-        self._detection_thread.start()
+    def __init__(self, video_frame: tk.Label) -> None:
+        self._run_detections: bool = True
+        self._video_frame: tk.Label = video_frame
+        self._current_frame: ImageTk.PhotoImage = ImageTk.PhotoImage(Image.open('./images/video_frame_default.gif'))
+        self._video_frame.after(0, self._detect, cv2.VideoCapture(0, cv2.CAP_DSHOW))
 
-    def _detect(self) -> None:
-        pass
+    @property
+    def run_detections(self) -> bool:
+        return self._run_detections
 
-    def _to_photo_image(self, rgb_array: np.ndarray) -> PhotoImage:
-        pass
+    @run_detections.setter
+    def run_detections(self, run_detections: bool) -> None:
+        self._run_detections = run_detections
 
-    def _update_frame(self, final_img: PhotoImage) -> None:
-        pass
+    def _detect(self, webcam_feed: cv2.VideoCapture) -> None:
+        if self._run_detections:
+            img_array_with_detections: np.ndarray = self._analyze_image(webcam_feed)
+            self._current_frame: ImageTk.PhotoImage = self._to_photo_image(img_array_with_detections)
+            self._update_frame()
+
+        # recursively call _detect() method to perform detections on next frame
+        self._video_frame.after(1, self._detect, webcam_feed)
+
+    def _analyze_image(self, webcam_feed: cv2.VideoCapture) -> np.ndarray:
+        # read() returns a tuple of a read success flag and a 3D ndarray
+        flag, img = webcam_feed.read()
+
+        # shift color for proper multi-scale detection
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # find faces in the image using haar cascade .xml file and detectMultiScale() function
+        faces = FaceDetection.FACE_CASCADE.detectMultiScale(image=img_rgb, scaleFactor=1.1, minNeighbors=8)
+
+        # draw rectangle around detected face(s)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 0, 255), thickness=2, lineType=cv2.FILLED)
+
+        return img
+
+    def _to_photo_image(self, img_array: np.ndarray) -> tk.PhotoImage:
+        # color-shift image to standard RGB colorspace
+        shifted_img = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+
+        # mirror image for user convenience then
+        mirrored_img = ImageOps.mirror(image=Image.fromarray(shifted_img, mode="RGB")).resize((920, 520),
+                                                                                              Image.ANTIALIAS)
+
+        # convert mirrored_img to ImageTk.PhotoImage object
+        final_img = ImageTk.PhotoImage(image=mirrored_img)
+
+        return final_img
+
+    # def _update_frame(self, final_img: tk.PhotoImage) -> None:
+    def _update_frame(self):
+        self._video_frame.configure(image=self._current_frame)
 
 
 class HandDetection(Detection):
-    def __init__(self, video_frame: Label) -> None:
+    def __init__(self, video_frame: tk.Label) -> None:
+        self._run_detections = False
+
         self._detection_thread = Thread(target=self._detect)
         self._video_frame = video_frame
         self._detection_thread.start()
 
-    def _detect(self) -> None:
+    @property
+    def run_detections(self) -> bool:
+        return self._run_detections
+
+    @run_detections.setter
+    def run_detections(self, run_detections: bool) -> None:
+        self._run_detections = run_detections
+
+    def _detect(self, webcam_feed: cv2.VideoCapture) -> None:
         pass
 
-    def _to_photo_image(self, rgb_array: np.ndarray) -> PhotoImage:
+    def _analyze_image(self, webcam_feed: cv2.VideoCapture) -> np.ndarray:
         pass
 
-    def _update_frame(self, final_img: PhotoImage) -> None:
+    def _to_photo_image(self, img_array: np.ndarray) -> tk.PhotoImage:
+        pass
+
+    def _update_frame(self) -> None:
         pass
--- render.py
+++ render.py
@@ -1,10 +1,9 @@
-from tkinter import *
-
+import tkinter as tk
 from PIL import ImageTk, Image
-
 import detections
 
 
+# TODO: Make MainGUI inherit from tk.Frame class
 class MainGUI:
     """
     MainGUI class initializes the tkinter GUI window and handles event listening
@@ -26,7 +25,7 @@ class MainGUI:
     def __init__(self) -> None:
         """ Initializes Tkinter window and starts mainloop() """
         # init window
-        self._window: Tk = Tk()
+        self._window: tk.Tk = tk.Tk()
 
         # init PIL PhotoImage objects (to be displayed in labels)
         self._images: dict = {
@@ -42,23 +41,24 @@ class MainGUI:
 
         # init widgets
         self._buttons: dict = {
-            'face_button': Button(self._window, bg=MainGUI.COLORS['button_bg'], command=self._face_click,
-                                  fg=MainGUI.COLORS['button_fg'], text='Toggle Facial Recognition', width=0),
-            'hand_button': Button(self._window, bg=MainGUI.COLORS['button_bg'], command=self._hand_click,
-                                  fg=MainGUI.COLORS['button_fg'], text='Toggle Hand Recognition', width=0),
-            'exit_button': Button(self._window, bg=MainGUI.COLORS['button_bg'], command=self._exit_click,
-                                  fg=MainGUI.COLORS['button_fg'], text='Exit', width=15)
+            'face_button': tk.Button(self._window, bg=MainGUI.COLORS['button_bg'], command=self._face_click,
+                                     fg=MainGUI.COLORS['button_fg'], text='Toggle Facial Recognition', width=0),
+            'hand_button': tk.Button(self._window, bg=MainGUI.COLORS['button_bg'], command=self._hand_click,
+                                     fg=MainGUI.COLORS['button_fg'], text='Toggle Hand Recognition', width=0),
+            'exit_button': tk.Button(self._window, bg=MainGUI.COLORS['button_bg'], command=self._exit_click,
+                                     fg=MainGUI.COLORS['button_fg'], text='Exit', width=15)
         }
 
         self._labels: dict = {
-            'title_label': Label(self._window, bg=MainGUI.COLORS['label_bg'], fg=MainGUI.COLORS['label_fg'],
-                                 text='Welcome to the Recognition Suite!', font="none 12 bold"),
-            'face_image_label': Label(self._window, image=self._images['face_image'], height=0, width=0, borderwidth=2,
-                                      bg=MainGUI.COLORS['image_bg']),
-            'hand_image_label': Label(self._window, image=self._images['hand_image'], height=0, width=0, borderwidth=2,
-                                      bg=MainGUI.COLORS['image_bg']),
-            'video_frame_label': Label(self._window, image=self._images['video_frame_default_image'],
-                                       bg=MainGUI.COLORS['image_bg'], height=520, width=920, borderwidth=2)
+            'title_label': tk.Label(self._window, bg=MainGUI.COLORS['label_bg'], fg=MainGUI.COLORS['label_fg'],
+                                    text='Welcome to the Recognition Suite!', font='none 20 bold'),
+            'face_image_label': tk.Label(self._window, image=self._images['face_image'], height=0, width=0,
+                                         borderwidth=2, bg=MainGUI.COLORS['image_bg']),
+            'hand_image_label': tk.Label(self._window, image=self._images['hand_image'], height=0, width=0,
+                                         borderwidth=2, bg=MainGUI.COLORS['image_bg']),
+            # TODO: change dimensions of video frame to be closer to 16:9
+            'video_frame_label': tk.Label(self._window, image=self._images['video_frame_default_image'],
+                                          bg=MainGUI.COLORS['image_bg'], height=520, width=920, borderwidth=2),
         }
 
         # additional window configuration and widget placement
@@ -71,22 +71,24 @@ class MainGUI:
 
     def _place_elements(self) -> None:
         # ROW 0
-        self._labels['title_label'].grid(row=0, column=0, sticky=W)  # title label
+        self._labels['title_label'].grid(row=0, column=0, padx=(25, 0), sticky=tk.W)  # title label
         # ROW 1
-        self._labels['face_image_label'].grid(row=1, column=0, pady=(10, 10), sticky=W)  # face image
-        self._labels['hand_image_label'].grid(row=1, column=0, pady=(10, 10), sticky=N)  # hand image
+        self._labels['face_image_label'].grid(row=1, column=0, padx=(25, 0), pady=(10, 10), sticky=tk.W)  # face img
+        self._labels['hand_image_label'].grid(row=1, column=0, pady=(10, 10), sticky=tk.N)  # hand img
         # ROW 2
-        self._buttons['face_button'].grid(row=2, column=0, pady=(0, 20), sticky=W)  # face detection button
-        self._buttons['hand_button'].grid(row=2, column=0, pady=(0, 20), sticky=N)  # hand detection button
+        self._buttons['face_button'].grid(row=2, column=0, padx=(25, 0), pady=(0, 20), sticky=tk.W)  # face detection bt
+        self._buttons['hand_button'].grid(row=2, column=0, sticky=tk.N)  # hand detection bt
         # ROW 3
-        self._labels['video_frame_label'].grid(row=3, column=0, padx=(50, 50), pady=(50, 50), sticky=N)  # video frame
+        self._labels['video_frame_label'].grid(row=3, column=0, padx=(25, 25), pady=(25, 0), sticky=tk.N)  # video frame
         # ROW 4
-        self._buttons['exit_button'].grid(row=4, column=0, padx=(50, 0), sticky=W)  # exit button
+        self._buttons['exit_button'].grid(row=4, column=0, padx=(25, 0), pady=(15, 10), sticky=tk.NW)  # exit bt
 
     def _face_click(self) -> None:
+        # TODO: Implement toggling functionality with @run_detections property
         face_detection = detections.FaceDetection(video_frame=self._labels['video_frame_label'])
 
     def _hand_click(self) -> None:
+        # TODO: Implement toggling functionality with @run_detections property
         pass
 
     def _exit_click(self) -> None:
